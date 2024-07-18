b0e906384148102f459335f43758ccbe3da959ea
Nguyen Dinh Thang <tn2k2tb@gmail.com>
2024-07-18 15:09:28 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test
+          <h1>Test</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
